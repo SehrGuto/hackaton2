3bd8e8b80afe6997b59d29a86b5dcb4dd59a58e6
Augusto Pavani <pavani.augusto@gmail.com>
2024-02-08 09:59:49 -0500

    creación del proyecto y de la clase contacto

---
+++ AgendaTelefonica/src/main/java/com/mycompany/agendatelefonica/Contacto.java
@@ -0,0 +1,63 @@
+package com.mycompany.agendatelefonica;
+
+public class Contacto {
+    public String nombre;
+    public int telefono; 
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre, int telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    
+    public void aniadirContacto(){
+        
+    }
+    
+    public void existeContacto(){
+        
+    }
+    
+    public void listarContactos(){
+        
+    }
+    
+    public void buscaContactos(){
+        
+    }
+    
+    public void eliminarContacto(){
+        
+    }
+    
+    public void agendaLlena(){
+        
+    }
+    
+    public void espacioLibre(){
+        
+    }
+    
+    
+}
+
+
+
